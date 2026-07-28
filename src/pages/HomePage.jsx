eabc535eecd287f334e7a7f01176376ab38a24a5
Ehab Mohamed <ehabmohsen66@gmail.com>
2026-07-28 17:28:47 +0300

    Restore interactive Agency Orchestration Card to Hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -157,6 +157,16 @@ export const HomePage = ({ setActiveTab, onSelectProject, onOpenPlanner }) => {
               ))}
             </motion.div>
 
+            {/* Interactive Hero Agency Orchestration Engine Card */}
+            <motion.div
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 1.1 }}
+              className="pt-10 max-w-5xl mx-auto text-left"
+            >
+              <HeroAgencyOrchestrationCard onOpenPlanner={onOpenPlanner} />
+            </motion.div>
+
           </div>
         </div>
       </section>
